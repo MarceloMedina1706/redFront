1d2bd0ae873416fe5fc4dd4f393b8ac907338073
Marcelo Medina <marce_me_di_na@hotmail.com>
2021-11-01 15:18:29 -0300

    creacion de algunos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { PostHeaderComponent } from './components/post-header/post-header.component';
+import { PostBodyComponent } from './components/post-body/post-body.component';
+import { PostFooterComponent } from './components/post-footer/post-footer.component';
+import { PostComentariosComponent } from './components/post-comentarios/post-comentarios.component';
+import { ComentarioComponent } from './components/post-comentarios/comentario/comentario.component';
+import { NavComponent } from './components/nav/nav.component';
+import { NavSearchComponent } from './components/nav-search/nav-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostHeaderComponent,
+    PostBodyComponent,
+    PostFooterComponent,
+    PostComentariosComponent,
+    ComentarioComponent,
+    NavComponent,
+    NavSearchComponent
   ],
   imports: [
     BrowserModule,
